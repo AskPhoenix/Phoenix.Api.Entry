03ce24619b338a75aa5fa1471311d18230922ab3
Théophile Spyrou <theofilos.spyrou@gmail.com>
2022-07-26 16:40:22 +0300

    Update Program class and Use Authorization

--- Phoenix.Api.Entry/Program.cs
+++ Phoenix.Api.Entry/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Phoenix.DataHandle.Api;
 using Phoenix.DataHandle.Identity;
 using Phoenix.DataHandle.Main.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure Web Host Defaults
+builder.WebHost.ConfigureKestrel(options => options.AddServerHeader = false);
 
 // Add services to the container.
 Action<DbContextOptionsBuilder> buildDbContextOptions = o => o
@@ -18,10 +26,26 @@ builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
     .AddUserManager<ApplicationUserManager>()
     .AddEntityFrameworkStores<ApplicationContext>();
 
-// Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateLifetime = true,
+            ValidateAudience = false,
+            ValidateIssuer = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+builder.Services.AddApplicationInsightsTelemetry(
+    o => o.ConnectionString = builder.Configuration["ApplicationInsights:ConnectionString"]);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddHttpsRedirection(options => options.HttpsPort = 443);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(o =>
 {
@@ -30,23 +54,63 @@ builder.Services.AddSwaggerGen(o =>
     // SwaggerDoc name refers to the name of the documention and is included in the endpoint path
     o.SwaggerDoc("v3", new Microsoft.OpenApi.Models.OpenApiInfo()
     {
-        Title = "Entry API",
+        Title = "Pavo API",
         Description = "A Rest API for the school data entry.",
         Version = "3.0"
     });
+
+
+    var jwtSecurityScheme = new OpenApiSecurityScheme
+    {
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Description = "Enter the JWT Bearer token.",
+        In = ParameterLocation.Header,
+        Name = "JWT Authentication",
+        Type = SecuritySchemeType.Http,
+
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+    o.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+    o.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { jwtSecurityScheme, Array.Empty<string>() }
+    });
+
+    o.SchemaFilter<SwaggerExcludeFilter>();
 });
 
-// TODO: Log to file
-builder.Logging.AddSimpleConsole(o => o.SingleLine = true);
+// Configure Logging
+// TODO: Create File Logging & on app insights
+builder.Logging.ClearProviders()
+    .AddConfiguration(builder.Configuration.GetSection("Logging"))
+    .SetMinimumLevel(LogLevel.Trace)
+    .AddSimpleConsole()
+    .AddDebug();
+
 
 var app = builder.Build();
 
-// TODO: Add Authentication
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    // app.UseDatabaseErrorPage();
+}
+else
+{
+    app.UseHsts();
+}
 
 // Configure the HTTP request pipeline.
 // if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
 app.UseSwagger();
-app.UseSwaggerUI(o => o.SwaggerEndpoint("/swagger/v3/swagger.json", "Entry v3"));
+app.UseSwaggerUI(o => o.SwaggerEndpoint("/swagger/v3/swagger.json", "Pavo v3"));
 
 app.UseHttpsRedirection();
 
