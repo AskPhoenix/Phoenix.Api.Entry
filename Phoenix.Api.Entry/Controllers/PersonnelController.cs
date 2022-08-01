39b211e515f9114a1659a9174100bc4412cad465
Théophile Spyrou <theofilos.spyrou@gmail.com>
2022-08-01 17:11:29 +0300

    Create Personnel Controller

--- Phoenix.Api.Entry/Controllers/PersonnelController.cs
+++ Phoenix.Api.Entry/Controllers/PersonnelController.cs
@@ -1,10 +1,11 @@
 ﻿using Phoenix.DataHandle.Api.Types;
 using Phoenix.DataHandle.Main.Types;
+using System.ComponentModel.DataAnnotations;
 
 namespace Phoenix.Api.Entry.Controllers
 {
     [ApiExplorerSettings(GroupName = "4")]
-    public class PersonnelController : UserController
+    public class PersonnelController : UserEntryController<PersonnelRoleRankApi>
     {
         public PersonnelController(
             PhoenixContext phoenixContext,
@@ -16,18 +17,26 @@ namespace Phoenix.Api.Entry.Controllers
 
         #region POST
 
-        [HttpPost]
-        public async Task<ApplicationUserApi?> PostAsync([FromBody] ApplicationUserApi appUserApi,
-            PersonnelRoleRankApi role)
+        public override async Task<ApplicationUserApi?> PostAsync([FromBody] ApplicationUserApi appUserApi,
+            PersonnelRoleRankApi role, [FromQuery, Required] int[] school_ids)
         {
-            // TODO: School, Courses
+            _logger.LogInformation("Entry -> Personnel -> Post");
 
-            _logger.LogInformation("Entry -> Client -> Post");
-
-            if (!CheckUserAuth())
+            if ((int)role < 0 || (int)role >= RoleExtensions.StaffRoleRanks.Length)
                 return null;
 
-            var roleRank = (RoleRank)(role + RoleHierarchy.StaffRolesBase);
+            List<School> schools = new(school_ids.Count());
+            foreach (var schoolId in school_ids)
+            {
+                var school = this.FindSchool(schoolId);
+                if (school is not null)
+                    schools.Add(school);
+            }
+
+            if (!schools.Any())
+                return null;
+
+            var roleRank = PersonnelRoleRankApiExtensions.ConvertToRoleRank(role);
 
             var appUser = appUserApi.ToAppUser();
             appUser.Id = 0;
@@ -40,45 +49,54 @@ namespace Phoenix.Api.Entry.Controllers
             user.IsSelfDetermined = true;
             user.DependenceOrder = 0;
 
+            foreach (var school in schools)
+                user.Schools.Add(school);
+
             user = await _userRepository.CreateAsync(user);
 
-            return new ApplicationUserApi(user, appUser);
+            return new ApplicationUserApi(user, appUser, new(1) { roleRank });
         }
 
         #endregion
 
         #region GET
 
-        [HttpGet]
-        public override IEnumerable<ApplicationUserApi>? Get()
+        public override async Task<IEnumerable<ApplicationUserApi>?> GetAsync(PersonnelRoleRankApi role)
         {
-            throw new NotImplementedException();
-        }
+            _logger.LogInformation("Entry -> Personnel -> Get -> {role}", role.ToString());
 
-        [HttpGet("{id}")]
-        public override ApplicationUserApi? Get(int id)
-        {
-            throw new NotImplementedException();
+            var roleRank = PersonnelRoleRankApiExtensions.ConvertToRoleRank(role);
+
+            return (await this.GetAsync())?.Where(au => au.Roles.Any(r => r.ToRoleRank() == roleRank));
         }
 
         #endregion
 
         #region PUT
 
-        [HttpPut]
-        public override Task<ApplicationUserApi?> PutAsync(int id, [FromBody] ApplicationUserApi appUserApi)
+        public override async Task<ApplicationUserApi?> PutAsync(int id,
+            [FromBody] ApplicationUserApi appUserApi)
         {
-            throw new NotImplementedException();
-        }
+            _logger.LogInformation("Entry -> Personnel -> Put -> {id}", id);
 
-        #endregion
+            var user = FindUser(id);
+            if (user is null)
+                return null;
 
-        #region DELETE
+            var appUser = await _userManager.FindByIdAsync(user.AspNetUserId.ToString());
 
-        [HttpDelete]
-        public override Task<IActionResult> DeleteAsync(int id)
-        {
-            throw new NotImplementedException();
+            user = appUserApi.User.ToUser(user);
+            appUser = appUserApi.ToAppUser(appUser);
+
+            user.IsSelfDetermined = true;
+            user.DependenceOrder = 0;
+
+            user = await _userRepository.UpdateAsync(user);
+            await _userManager.UpdateAsync(appUser);
+
+            var roleRanks = await _userManager.GetRoleRanksAsync(appUser);
+
+            return new ApplicationUserApi(user, appUser, roleRanks.ToList());
         }
 
         #endregion
--- Phoenix.Api.Entry/Controllers/UserEntryController.cs
+++ Phoenix.Api.Entry/Controllers/UserEntryController.cs
@@ -17,34 +17,137 @@ namespace Phoenix.Api.Entry.Controllers
         {
         }
 
+        #region POST
 
         [HttpPost("{role}")]
         public abstract Task<ApplicationUserApi?> PostAsync([FromBody] ApplicationUserApi appUserApi,
             TRoleRank role, [FromQuery, Required] int[] school_ids);
 
+        #endregion
+
+        #region GET
+
         [HttpGet]
-        public abstract Task<IEnumerable<ApplicationUserApi>?> GetAsync();
+        public async Task<IEnumerable<ApplicationUserApi>?> GetAsync()
+        {
+            _logger.LogInformation("Entry -> User -> Get");
+
+            var users = this.PhoenixUser?.Schools.SelectMany(s => s.Users);
+            if (users is null)
+                return null;
+
+            return await this.GetApplicationUsersAsync(users);
+        }
 
         [HttpGet("{role}")]
-        public abstract IEnumerable<ApplicationUserApi>? Get(TRoleRank role);
+        public abstract Task<IEnumerable<ApplicationUserApi>?> GetAsync(TRoleRank role);
 
         [HttpGet("{id}")]
-        public abstract ApplicationUserApi? Get(int id);
+        public async Task<ApplicationUserApi?> GetAsync(int id)
+        {
+            _logger.LogInformation("Entry -> User -> Get -> {id}", id);
+
+            var user = FindUser(id);
+            if (user is null)
+                return null;
+
+            var appUser = await _userManager.FindByIdAsync(user.AspNetUserId.ToString());
+            var roleRanks = await _userManager.GetRoleRanksAsync(appUser);
+
+            return new ApplicationUserApi(user, appUser, roleRanks.ToList());
+        }
+
+        [HttpGet("{id}/schools")]
+        public IEnumerable<SchoolApi>? GetSchools(int id)
+        {
+            _logger.LogInformation("Entry -> User -> Get -> Schools -> {id}", id);
+
+            var user = FindUser(id);
+            if (user is null)
+                return null;
+
+            return user.Schools
+                .Select(s => new SchoolApi(s));
+        }
+
+        [HttpGet("{id}/courses")]
+        public IEnumerable<CourseApi>? GetCourses(int id)
+        {
+            _logger.LogInformation("Entry -> User -> Get -> Courses -> {id}", id);
+
+            var user = FindUser(id);
+            if (user is null)
+                return null;
+
+            return user.Courses
+                .Select(c => new CourseApi(c));
+        }
+
+        #endregion
+
+        #region PUT
 
         [HttpPut("{id}")]
         public abstract Task<ApplicationUserApi?> PutAsync(int id, [FromBody] ApplicationUserApi appUserApi);
 
         [HttpPut("{id}/courses")]
-        public abstract Task<IEnumerable<CourseApi>?> PutCoursesAsync(int id, [FromBody] List<int> courseIds);
+        public async Task<IEnumerable<CourseApi>?> PutCoursesAsync(int id, [FromBody] List<int> courseIds)
+        {
+            _logger.LogInformation("Entry -> User -> Put -> Courses -> {id}", id);
+
+            var user = FindUser(id);
+            if (user is null)
+                return null;
+
+            user.Courses.Clear();
+
+            Course? course;
+            foreach (var courseId in courseIds)
+            {
+                course = this.FindCourse(courseId);
+                if (course is null)
+                    continue;
+
+                user.Courses.Add(course);
+            }
+
+            user = await _userRepository.UpdateAsync(user);
+
+            return user.Courses.Select(c => new CourseApi(c));
+        }
+
+        #endregion
+
+        #region DELETE
 
         [HttpDelete("{id}")]
-        public abstract Task<IActionResult> DeleteAsync(int id);
-
-        protected User? FindUser(int userId)
+        public async Task<IActionResult> DeleteAsync(int id)
         {
-            return this.PhoenixUser?.Schools
-                .SelectMany(s => s.Users)
-                .SingleOrDefault(u => u.AspNetUserId == userId);
+            _logger.LogInformation("Entry -> User -> Delete -> {id}", id);
+
+            if (!CheckUserAuth())
+                return Unauthorized();
+
+            var user = FindUser(id);
+            if (user is null)
+                return BadRequest();
+
+            await _userRepository.DeleteAsync(user);
+
+            return Ok();
+        }
+
+        #endregion
+
+        protected int CalculateDependanceOrder(User parent)
+        {
+            if (parent is null)
+                throw new ArgumentNullException(nameof(parent));
+
+            return parent.Children
+                .Select(c => c.DependenceOrder)
+                .DefaultIfEmpty(0)
+                .Max() + 1;
         }
     }
 }
